27ad6f2be8bd2ae3388258bc853b5ee03c6eb458
deal-peer <info@dealpeer.com>
2018-10-31 13:22:38 +0300

    Refactoring

--- src/components/Callback/index.js
+++ src/components/Callback/index.js
@@ -2,25 +2,58 @@ import React, {Component} from 'react';
 import Wrapper from '../Wrapper/index';
 import Input from '../Input/index';
 import SimpleButton from '../SimpleButton/index';
+import * as Api from '../../api';
 import './styles.css';
 export default class Callback extends Component {
-    onClickHandle() {
-        alert('Call me');
+    state = {
+        phone: '',
+        isSuccess: false
+    };
+    async onClickHandle() {
+        const {phone} = this.state;
+        let phoneRe = /^[+0-9-() ]+$/;
+
+        if (!phoneRe.test(phone)) {
+            return;
+        }
+        try {
+            await Api.sentCallbackRequest(phone);
+            this.setState({isSuccess: true});
+        } catch (err) {
+            console.error(err);
+        }
     }
+
+    onCloseHandle() {
+        this.props.nav('button');
+    }
+
+    onChange(e) {
+        this.setState({
+            phone: e.target.value
+        });
+    }
+
     render() {
         const {nav, color} = this.props;
+        const {isSuccess, phone} = this.state;
 
         return <Wrapper nav={nav} title="Обратный звонок">
-            <div className="callback">
+            {!isSuccess ? <div className="callback">
                 <p className="info">Укажите ваш контактный телефон и мы свяжемся с вами 
                 в течении нескольких минут</p>
                 <div style={{width: '80%'}}>
-                    <Input title="Телефон" placeholder="+7 (999) 999-99-99"/>
+                    <Input value={phone} onChange={this.onChange.bind(this)} title="Телефон" placeholder="+7 (999) 999-99-99"/>
                 </div>
                 <div style={{width: '80%', marginTop: '20px'}}>
                     <SimpleButton title="Позвонить" onClick={this.onClickHandle.bind(this)} color={color} />
                 </div>
-            </div>
+            </div> : <div className="callback">
+                <div style={{width: '80%'}}>
+                    <p className="info">Спасибо, наш оператор перезвонит Вам через несколько минут.</p>
+                    <SimpleButton title="Закрыть" onClick={this.onCloseHandle.bind(this)} color={color} />
+                </div>
+            </div>}
         </Wrapper>
     }
 }

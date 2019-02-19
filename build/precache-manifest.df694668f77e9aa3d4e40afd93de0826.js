0db2dfbef2fe4124db7561fcc78d37af82b19882
potorochinau <3dspaun@gmail.com>
2019-02-19 15:22:53 +0300

    - add build to repo.

---
+++ build/precache-manifest.df694668f77e9aa3d4e40afd93de0826.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "461488fef9f73ad29037",
+    "url": "/widget.js"
+  },
+  {
+    "revision": "463a7b01c12e6005148347e750205593",
+    "url": "/index.html"
+  }
+];
